f164a924f497c96911ec049e230961cd93223d5a
antoniogamizdelgado <antonio.gamiz@badgermapping.com>
2022-10-15 12:54:10 +0200

    feat(implementation):

---
+++ src/README.md
@@ -0,0 +1,19 @@
+## Any2Vec Model
+
+To get the dataset you can run:
+
+~~~bash
+poetry run get_data_set
+~~~
+
+To clean the datasets you can run:
+
+~~~bash
+poetry run clean
+~~~
+
+To run the tests you can run:
+
+~~~bash
+poetry run test
+~~~
---
+++ src/scripts/get_data_set.py
@@ -0,0 +1,43 @@
+import glob
+import zipfile
+import requests
+import os
+
+DATASET_DIR_PATH = '../datasets'
+PATH_TO_ZIP_FILE = os.path.join(DATASET_DIR_PATH, 'dataset.zip')
+
+DATASET_NEW_FILE_NAME = 'dataset.json'
+DATASET_NEW_PATH = os.path.join(DATASET_DIR_PATH, DATASET_NEW_FILE_NAME)
+
+DEFAULT_DATASET_URL = 'https://github.com/rhgarcia/tropescraper/raw/master/datasets/tvtropes_20200302.json.zip'
+
+
+def main():
+    clean()
+    url = os.getenv('DATASET_URL', DEFAULT_DATASET_URL)
+    _download_file(url, PATH_TO_ZIP_FILE)
+    _extract_zip_file(PATH_TO_ZIP_FILE, DATASET_DIR_PATH)
+    os.remove(PATH_TO_ZIP_FILE)
+    original_dataset_name = _get_dataset_json_file()
+    os.rename(original_dataset_name, DATASET_NEW_PATH)
+
+
+def _download_file(url, save_to):
+    response = requests.get(url)
+    with open(save_to, 'wb')as f:
+        f.write(response.content)
+
+
+def _extract_zip_file(zip_file_path, extract_to_path):
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to_path)
+
+
+def _get_dataset_json_file():
+    return glob.glob(f'{DATASET_DIR_PATH}/*.json')[0]
+
+
+def clean():
+    files = glob.glob(f'{DATASET_DIR_PATH}/*.*')
+    for f in files:
+        os.remove(f)
